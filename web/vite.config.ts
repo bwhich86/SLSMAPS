9178bfbfcf82e08e6e635634a5d8fc2bcbe8a31d
bwhich86 <bwhich86@gmail.com>
2026-03-02 23:38:53 -0500

    Change base path for Vite configuration

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // IMPORTANT: set base to your repo name for GitHub Pages, e.g. '/lighting-inspections/'
 export default defineConfig({
-  base: (process.env.VITE_BASE || '/'),
+  base: (process.env.VITE_BASE || 'SLSMAPS'),
   plugins: [react()],
   server: { port: 5173 }
 });
